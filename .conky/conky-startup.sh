245d0e221ef9aa94f6817de88f56479149e56854
serii <seriiburduja@gmail.com>
2018-07-18 17:13:51 +0300

    add conky and cherry tree

---
+++ .conky/conky-startup.sh
@@ -0,0 +1,4 @@
+sleep 20s
+killall conky
+cd "/home/serii/.conky/Conky long_a.ovcharov/Conky long_a.ovcharov"
+conky -c "/home/serii/.conky/Conky long_a.ovcharov/Conky long_a.ovcharov/conky long_a.ovcharov" &
